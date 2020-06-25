711a055ceaa6f4866f92c22e65cc0f6fb3a307f4
setojean <miku_kazuyoshi@icloud.com>
2020-06-25 23:37:49 +0900

    Inital commit

---
+++ App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });  
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} /> 
+
+      <Text style={styles.test}>COUNTER</Text>
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    padding: '24px',
+    borderBox: 'box-sizing',
+  },
+  test: {
+    color: 'rgba(0,0,0,0.87)',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 24,
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    padding: '24px',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
+});
